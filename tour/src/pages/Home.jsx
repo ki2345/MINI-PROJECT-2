95c700371c799b89c52619cec6e582acdd6fe522
sandeep kumar <sandeep.kumar_cs20@gla.ac.in>
2023-04-08 22:21:09 -0400

    searchBAr added

--- tour/src/pages/Home.jsx
+++ tour/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
 import Subtitle from '../shared/Subtitle';
 import worldImg from '../assets/images/world.png';
+import SearchBar from '../shared/SearchBar'
 
 const Home = () => {
   return (
@@ -40,6 +41,8 @@ const Home = () => {
                 <img src={heroImg02} alt="" />
                 </div>  
             </Col> 
+
+            <SearchBar/>
           </Row>
         </container>
       </section>
