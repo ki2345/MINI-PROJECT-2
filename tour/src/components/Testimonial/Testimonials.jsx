c341ea4f63825e6ecbfad27e7ebec1935bc08233
rishika570 <rishikasingh570@gmail.com>
2023-04-10 00:12:28 +0530

    testimonial added

--- tour/src/components/Testimonial/Testimonials.jsx
+++ tour/src/components/Testimonial/Testimonials.jsx
@@ -5,11 +5,82 @@ import ava02 from '../assets/images/ava-02.jpg';
 import ava03 from '../assets/images/ava-03.jpg';
 
 const Testimonials = () => {
-  return <Slider>
-    <div>
-       <div className='testimonial py-4 px-3'>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam aliquid culpa quas unde officia fugiat dignissimos asperiores est consequatur ipsum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, consequuntur!you</p>
-       </div>
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    swipeToSlide: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+
+  };
+  return <Slider {...settings}>
+    <div className='testimonial py-4 px-3'>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam aliquid culpa quas unde officia fugiat dignissimos asperiores est consequatur ipsum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, consequuntur!you</p>
+
+      <div className='d-flex align-items-center gap-4 mt-3'>
+        <img src={ava01} className='w-25 h-25 rounded-2' alt="" />
+        <div>
+          <h6 className='mb-0 mt-3'>John Doe</h6>
+          <p>Customer</p>
+        </div>
+      </div>
+    </div>
+
+    <div className='testimonial py-4 px-3'>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam aliquid culpa quas unde officia fugiat dignissimos asperiores est consequatur ipsum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, consequuntur!you</p>
+
+      <div className='d-flex align-items-center gap-4 mt-3'>
+        <img src={ava02} className='w-25 h-25 rounded-2' alt="" />
+        <div>
+          <h6 className='mb-0 mt-3'>John Doe</h6>
+          <p>Customer</p>
+        </div>
+      </div>
+    </div>
+
+    <div className='testimonial py-4 px-3'>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam aliquid culpa quas unde officia fugiat dignissimos asperiores est consequatur ipsum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, consequuntur!you</p>
+
+      <div className='d-flex align-items-center gap-4 mt-3'>
+        <img src={ava03} className='w-25 h-25 rounded-2' alt="" />
+        <div>
+          <h6 className='mb-0 mt-3'>John Doe</h6>
+          <p>Customer</p>
+        </div>
+      </div>
+    </div>
+
+    <div className='testimonial py-4 px-3'>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam aliquid culpa quas unde officia fugiat dignissimos asperiores est consequatur ipsum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, consequuntur!you</p>
+
+      <div className='d-flex align-items-center gap-4 mt-3'>
+        <img src={ava02} className='w-25 h-25 rounded-2' alt="" />
+        <div>
+          <h6 className='mb-0 mt-3'>John Doe</h6>
+          <p>Customer</p>
+        </div>
+      </div>
     </div>
   </Slider>
 }
---
+++ tour/src/shared/Newsletter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './newsletter.css'
+const Newsletter = () => {
+  return (
+    <div>
+      Newsletter
+    </div>
+  )
+}
+
+export default Newsletter
