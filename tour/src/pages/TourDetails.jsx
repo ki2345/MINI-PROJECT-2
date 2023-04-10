4b90881a4c32385fc77b3ea5890a779776fa0631
ki2345 <reddy2002kiran@gmail.com>
2023-04-10 22:30:28 +0530

    tour page added

--- tour/src/pages/TourDetails.jsx
+++ tour/src/pages/TourDetails.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import "../styles/tour-details.css";
+import { Container, Row, Col, Form, ListGroup } from "reactstrap";
+import { useParams } from "react-router-dom";
+import tourData from "../assets/data/tour";
+
 
 const TourDetails = () => {
+  const { id } = useParams();
+
+  //this is a static data later we will call our API and load our data from database
+  const tour = tourData.find((tour) => tour.id === id);
+
+  //destructive properties from tour object
+  const { photo, title, desc, price, reviews, city, distance, maxGroupSize } =
+    tour;
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="8">
+              <div className="tour_content">
+                <img src={photo} alt="" />
+
+                <div className="tour_info">
+                  <h2>{title}</h2>
+
+                  <div className="d-flex align-items-center gap-5">
+                    
+
+                
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+};
 
 export default TourDetails;
--- tour/src/pages/Tours.jsx
+++ tour/src/pages/Tours.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import CommonSection from '../shared/CommonSection';
 import "../styles/tour.css";
 import tourData from '../assets/data/tour';
@@ -8,6 +8,15 @@ import Newsletter from '../shared/Newsletter';
 import { Container, Row, Col } from 'reactstrap';
 
 const Tours = () => {
+
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+      const pages = Math.ceil(5/ 4);
+      setPageCount(pages);
+  },[page]);
+
   return (
     <>
       <CommonSection title={"All Tours"}/>
@@ -18,16 +27,29 @@ const Tours = () => {
           </Row>
         </Container>
       </section>
-      <section className='pt-0'>
+      <section className="pt-0">
         <Container>
           <Row>
               {
-                tourData ?.map(tour => <Col lg='3' key={tour.id}>
+                tourData ?.map(tour => <Col lg='3' className='mb-4' key={tour.id}>
                   <TourCard tour={tour} /></Col>)
               }
+              <Col lg='12'>
+                <div className="pagination d-flex align-items-center
+                 justify-content-center mt-4 gap-3">
+                    {[...Array(pageCount).keys()].map(number => (
+                      <span key={number} onClick={() => setPage(number)}
+                      className={page === number ? 'active_page' : ""}
+                      >
+                        {number + 1}
+                      </span>
+                    ))}
+                </div>
+              </Col>
           </Row>
         </Container>
       </section>
+      <Newsletter/>
     </>
   )
 }
