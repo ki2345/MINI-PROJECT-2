eaef86b09ad1837d75aac169bcc53bf6b9c12740
sandeep kumar <sandeep.kumar_cs20@gla.ac.in>
2023-04-12 18:16:16 -0400

    login and register page added

--- tour/src/pages/TourDetails.jsx
+++ tour/src/pages/TourDetails.jsx
@@ -6,7 +6,7 @@ import tourData from "../assets/Data/tour";
 import calculateAvgRating from "../utils/avgRating";
 import avatar from "../assets/images/avatar.jpg";
 import Booking from "../components/Booking/Booking";
-
+import NewsLetter from '../shared/Newsletter'
 const TourDetails = () => {
   const { id } = useParams();
   const reviewMsgRef=useRef('')
@@ -152,6 +152,7 @@ const TourDetails = () => {
           </Row>
         </Container>
       </section>
+      <NewsLetter />
     </>
   );
 };
