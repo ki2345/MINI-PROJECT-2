312f0acb39eb187a41a6f7e8de4fd0fd19800321
rishika570 <rishikasingh570@gmail.com>
2023-04-15 13:08:42 +0530

    controller added

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -11,3 +11,73 @@ export const createTour=async (req,res)=>{
         res.status(500).json({success:false,message:'Failed to create. Try again'});
     }
 };
+
+//update tour
+export const updateTour=async(req,res)=>{
+    const id=req.params.id
+    try{
+
+      const updatedTour=await Tour.findByIdAndUpdate(id,{
+        $set:req.body
+      },{new:true})
+      
+      res.status(200).json({
+        success:true,
+        message:'Successfully updated',data:updatedTour});
+
+    } catch(err){
+        res.status(500).json({
+            success:false,
+            message:'failed to update'});
+    }
+};
+//delete tour
+export const deleteTour=async(req,res)=>{
+    const id=req.params.id
+    try{
+
+      await Tour.findByIdAndDelete(id)
+      
+      res.status(200).json({
+        success:true,
+        message:'Successfully deleted'});
+
+    } catch(err){
+        res.status(500).json({
+            success:false,
+            message:'failed to delete'});
+    }
+};
+//getSingle tour
+export const getSingleTour=async(req,res)=>{
+    const id=req.params.id
+    try{
+
+      const tour=await Tour.findById(id)
+      
+      res.status(200).json({
+        success:true,
+        message:'Successfull',
+        data:tour});
+
+    } catch(err){
+        res.status(404).json({
+            success:false,
+            message:'not found'});
+    }
+};
+//getAll tour
+export const getAllTour=async(req,res)=>{
+    //for pagination
+    const page=parseInt(req.query.page);
+    try{
+      const tours=await Tour.find({}).skip(page*8).limit(8);
+      res.status(200).json({success:true,
+        message:'Successful',
+        data:tour})
+    } catch(err){
+        res.status(404).json({
+            success:false,
+            message:'not found'});
+    }
+};
--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,9 +1,17 @@
 import express from 'express'
-import { createTour } from './../controllers/tourController.js'
+import { createTour, deleteTour, getAllTour, getSingleTour, updateTour } from './../controllers/tourController.js'
 
 const router = express.Router()
 
 //create new tour
 router.post('/',createTour)
+//update new tour
+router.put('/:id',updateTour)
+//delete tour
+router.delete('/:id',deleteTour)
+//get single tour
+router.get('/:id',getSingleTour)
+//get all tour
+router.get('/',getAllTour)
 
 export default router;
