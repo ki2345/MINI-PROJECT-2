e97776af505def336b9eda82a15f07de25a47fde
ki2345 <reddy2002kiran@gmail.com>
2023-05-04 22:59:04 +0530

    project

--- tour/src/index.js
+++ tour/src/index.js
@@ -1,3 +1,8 @@
+
+// if (process.env.NODE_ENV != "production") {
+//   require("dotenv").config({ path: "./config.env" })
+// } 
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
